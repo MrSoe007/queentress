4eaf6151f6c8da2c1b643c1930b23f50e83f5591
MrSoe007 <soehtethtun6@gmail.com>
2026-03-09 23:43:10 +0630

    Update ui_manager.js

--- core/ui_manager.js
+++ core/ui_manager.js
@@ -1,7 +1,6 @@
 function showScreen(screenId) {
     const screens = document.querySelectorAll('.screen');
     screens.forEach(s => s.classList.remove('active'));
-    
     const target = document.getElementById(screenId);
     if (target) target.classList.add('active');
 }
